112a9e82d8df0d6ff9943addfff2f61b330f46fc
Pavel <zzzzfogzzzz@gmail.com>
2023-02-15 17:13:43 +0600

    100

---
+++ src/main/java/acmp/ACMP100.java
@@ -0,0 +1,16 @@
+package acmp;
+
+import java.util.Scanner;
+
+public class ACMP100 {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println(method(scanner));
+        }
+    }
+
+    private static String method(Scanner scanner) {
+        return " ";
+    }
+
+}
