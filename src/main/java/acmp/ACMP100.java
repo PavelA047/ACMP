80002e5ed5b7fb0fbfafe95c7a9e167fa44b9504
Pavel <zzzzfogzzzz@gmail.com>
2023-02-15 21:06:51 +0600

    100

--- src/main/java/acmp/ACMP100.java
+++ src/main/java/acmp/ACMP100.java
@@ -1,5 +1,7 @@
 package acmp;
 
+import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ACMP100 {
@@ -10,7 +12,33 @@ public class ACMP100 {
     }
 
     private static String method(Scanner scanner) {
-        return " ";
+        int number = scanner.nextInt();
+        BigInteger[][] arr = new BigInteger[number / 2][number / 2 * 9 + 1];
+        for (BigInteger[] bigIntegers : arr) {
+            Arrays.fill(bigIntegers, BigInteger.ZERO);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            arr[0][i] = BigInteger.ONE;
+        }
+
+        int plusNineIndex = 9;
+        for (int i = 1; i < arr.length; i++) {
+            plusNineIndex += 9;
+            for (int j = plusNineIndex; j >= 0; j--) {
+                for (int k = 0; k < 10; k++) {
+                    if (j - k < 0) break;
+                    arr[i][j] = arr[i][j].add(arr[i - 1][j - k]);
+                }
+            }
+        }
+
+        BigInteger retVal = BigInteger.ZERO;
+        for (BigInteger i : arr[number / 2 - 1]) {
+            retVal = retVal.add(i.pow(2));
+        }
+
+        return retVal.toString();
     }
 
 }
